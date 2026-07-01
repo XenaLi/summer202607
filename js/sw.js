d177dd309dfd4536f64cb3950a46ed9215ddb90e
XENALI-PC\user <infernodimon@hotmail.com>
2026-07-01 23:52:45 +0800

    fix: invalid service worker script
    + incorrect format & incomplete images of sw.js assets
    
    + update manifest

--- js/sw.js
+++ js/sw.js
@@ -1,8 +1,22 @@
 const CACHE_NAME = 'my-pwa-cache-v1';
 const ASSETS_TO_CACHE = [
   './',
-  'index.html'
+  'index.html',
+  'manifest.json',
+  'css/scanner.css',
+  'js/scanner.js',
+  'js/sw.js',
   'images/app_icon.png',
+  'images/gem=1.jpg',
+  'images/gem=2.jpg',
+  'images/gem=3.jpg',
+  'images/gem=4.jpg',
+  'images/gem=5.jpg',
+  'images/gem=6.jpg',
+  'images/gem=7.jpg',
+  'images/gem=8.jpg',
+  'images/gem=9.jpg',
+  'images/gem=10.jpg',
 ];
 
 self.addEventListener('install', (event) => {
